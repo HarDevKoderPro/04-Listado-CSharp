e47994a2d695f5026a2a8fb3006222e2cfd1eab2
HarDevKoderPro <hardevkoder@gmail.com>
2024-04-30 00:37:22 -0500

    Creacion del repositorio

---
+++ Principal.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace StudentsList
+{
+	partial class Principal
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+			this.label1 = new System.Windows.Forms.Label();
+			this.TxtEstudiante = new System.Windows.Forms.MaskedTextBox();
+			this.BtnIngresar = new System.Windows.Forms.Button();
+			this.LstTabla = new System.Windows.Forms.ListBox();
+			this.BtnEliminar = new System.Windows.Forms.Button();
+			this.BtnModificar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Estudiante:";
+			// 
+			// TxtEstudiante
+			// 
+			this.TxtEstudiante.Location = new System.Drawing.Point(78, 12);
+			this.TxtEstudiante.Name = "TxtEstudiante";
+			this.TxtEstudiante.Size = new System.Drawing.Size(139, 20);
+			this.TxtEstudiante.TabIndex = 1;
+			// 
+			// BtnIngresar
+			// 
+			this.BtnIngresar.Location = new System.Drawing.Point(223, 42);
+			this.BtnIngresar.Name = "BtnIngresar";
+			this.BtnIngresar.Size = new System.Drawing.Size(57, 27);
+			this.BtnIngresar.TabIndex = 2;
+			this.BtnIngresar.Text = "Ingresar";
+			this.BtnIngresar.UseVisualStyleBackColor = true;
+			this.BtnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
+			// 
+			// LstTabla
+			// 
+			this.LstTabla.FormattingEnabled = true;
+			this.LstTabla.Location = new System.Drawing.Point(15, 41);
+			this.LstTabla.Name = "LstTabla";
+			this.LstTabla.Size = new System.Drawing.Size(202, 186);
+			this.LstTabla.TabIndex = 3;
+			this.LstTabla.SelectedIndexChanged += new System.EventHandler(this.LstTabla_SelectedIndexChanged);
+			// 
+			// BtnEliminar
+			// 
+			this.BtnEliminar.Location = new System.Drawing.Point(223, 75);
+			this.BtnEliminar.Name = "BtnEliminar";
+			this.BtnEliminar.Size = new System.Drawing.Size(57, 27);
+			this.BtnEliminar.TabIndex = 4;
+			this.BtnEliminar.Text = "Eliminar";
+			this.BtnEliminar.UseVisualStyleBackColor = true;
+			this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+			// 
+			// BtnModificar
+			// 
+			this.BtnModificar.Location = new System.Drawing.Point(223, 108);
+			this.BtnModificar.Name = "BtnModificar";
+			this.BtnModificar.Size = new System.Drawing.Size(57, 27);
+			this.BtnModificar.TabIndex = 5;
+			this.BtnModificar.Text = "Editar";
+			this.BtnModificar.UseVisualStyleBackColor = true;
+			this.BtnModificar.Click += new System.EventHandler(this.BtnModificar_Click);
+			// 
+			// Principal
+			// 
+			this.AcceptButton = this.BtnIngresar;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(293, 237);
+			this.Controls.Add(this.BtnModificar);
+			this.Controls.Add(this.BtnEliminar);
+			this.Controls.Add(this.LstTabla);
+			this.Controls.Add(this.BtnIngresar);
+			this.Controls.Add(this.TxtEstudiante);
+			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Principal";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Listado de Estudiantes";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.MaskedTextBox TxtEstudiante;
+		private System.Windows.Forms.Button BtnIngresar;
+		private System.Windows.Forms.ListBox LstTabla;
+		private System.Windows.Forms.Button BtnEliminar;
+		private System.Windows.Forms.Button BtnModificar;
+	}
+}
+
---
+++ Principal.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+//-----------------------------------------------------------------------------------------------------------------------------------
+namespace StudentsList
+{
+	public partial class Principal : Form
+	{
+		//---------    Método que Recarga Listbox con los  nuevos datos ------------------------------------------
+		public void RefrescaListBox ()
+		{
+			//Refresco listBox para actualizar datos ingresados
+			LstTabla.DataSource = null;
+			//Ingreso dato al listBox
+			LstTabla.DataSource = estudiantes;
+		}
+		//------------------------------------------ ELEMENTOS GLOBALES ----------------------------------------------------
+		List<string> estudiantes = new List<string>();	// Lista de Estudiantes
+		public int posicion;										//Posición de Elemento seleccionado en ListBox
+		//-----------------------------------------------------------------------------------------------------------------------------------
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+		public Principal()
+		{
+			InitializeComponent();
+		}
+		//------------------------------------------- INGRESO DE DATOS ----------------------------------------------------------
+		private void BtnIngresar_Click(object sender, EventArgs e)
+		{
+			//capturar nombre de estudiante
+			string estudiante = TxtEstudiante.Text;
+			//valido que no esté vacio el Textbox
+			if (estudiante != "")
+			{
+				//Agrego el estudiante a la lista
+				estudiantes.Add(estudiante);
+				//Refresco listBox para actualizar datos ingresados
+				RefrescaListBox();
+			}
+			else
+			{
+				MessageBox.Show("No hay información para ingresar ...");
+			}
+			//limpiado de Textbox
+			TxtEstudiante.Clear();
+			//Asigno Foco a la caja de texto para escribir el nuevo dato
+			TxtEstudiante.Focus();
+		}
+		//------------------------------------------- ELIMINAR  DATOS -----------------------------------------------------------------
+		private void BtnEliminar_Click(object sender, EventArgs e)
+		{
+			//solo edita si hay un item seleccionado
+			if (LstTabla.SelectedIndex >= 0)
+			{
+				int posicion = LstTabla.SelectedIndex;
+				string item = estudiantes[posicion];
+				var res = MessageBox.Show("Desea Eliminar a  " + item + "?", "Eliminar Estudiante", MessageBoxButtons.OKCancel);
+				if (res == DialogResult.OK)
+				{
+					//Elimino elemento de la lista
+					estudiantes.RemoveAt(posicion);
+					//Refresco listBox para actualizar datos ingresados
+					RefrescaListBox();
+					//limpiado de Textbox
+					TxtEstudiante.Clear();
+				}
+			}
+			else
+			{
+				MessageBox.Show("Seleccione un Item para eliminar!");
+			}
+		}
+		//------------------------------------------- MODIFICAR  DATOS -----------------------------------------------------------------
+		private void BtnModificar_Click(object sender, EventArgs e)
+		{
+			BtnIngresar.Enabled = false;	
+			BtnEliminar.Enabled=false;
+			AcceptButton = BtnModificar;
+			if (BtnModificar.Text == "Editar")
+			{
+				//Obtengo la posición del item seleccionado
+				posicion = LstTabla.SelectedIndex;
+				//MessageBox.Show(posicion.ToString());
+				BtnModificar.Text = "Guardar";
+				if (LstTabla.SelectedIndex >= 0)
+				{
+					//Muestro valor seleccionado en el textbox
+					TxtEstudiante.Text = LstTabla.SelectedItem.ToString();
+					//Asigno Foco a la caja de texto para escribir el nuevo dato
+					TxtEstudiante.Focus();
+					//Coloco el cursor al final del contenido de la caja de texto
+					TxtEstudiante.SelectionStart = TxtEstudiante.Text.Length;
+				}
+			}
+			else
+			{
+				if (TxtEstudiante.Text != "")
+				{
+					//Restauro el nombre inicial del botón
+					BtnModificar.Text = "Editar";
+					//Elimino item seleccionado de la lista
+					estudiantes.RemoveAt(posicion);
+					//Inserto el nuevo dato en la lista
+					estudiantes.Insert(posicion, TxtEstudiante.Text);
+					//Refresco listBox para actualizar datos ingresados
+					RefrescaListBox();
+					//Limpio TextBox
+					TxtEstudiante.Clear();
+					//Habilito Botones
+					BtnIngresar.Enabled = true;
+					BtnEliminar.Enabled = true;
+					//Asigno enter a boton ingresar
+					AcceptButton = BtnIngresar ;
+					//Asigno Foco a la caja de texto para escribir el nuevo dato
+					TxtEstudiante.Focus();
+				}
+				else
+				{
+					MessageBox.Show("No se puede ingresar Dato Vacío ...");
+					TxtEstudiante.Text = LstTabla.SelectedItem.ToString();
+					//Asigno Foco a la caja de texto para escribir el nuevo dato
+					TxtEstudiante.Focus();
+					//Coloco el cursor al final del contenido de la caja de texto
+					TxtEstudiante.SelectionStart = TxtEstudiante.Text.Length;
+				}
+			}
+		}
+		public void LstTabla_SelectedIndexChanged(object sender, EventArgs e)
+		{
+		}
+	}
+}
